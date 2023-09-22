dfa2e375ac1397523ae449f8fc2a32fbe29ebf45
Misaki <tshimizumisaki@gmail.com>
2023-09-22 13:59:36 -0400

    Add header/navigation

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/navigation/Header';
+import './styles/sass/style.css';
 
 export interface IApplicationProps {}
 
 const Application: React.FunctionComponent<IApplicationProps> = (props) => {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,17 +1,116 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { PiBoneThin } from "react-icons/pi";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { PiBoneThin } from 'react-icons/pi';
+import { CiMenuBurger, CiShoppingCart } from 'react-icons/ci';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
-  return (
-    <header>
-      <nav className="navbar bg-body-tertiary">
-        <div>
-          <Link to="">
-            <PiBoneThin />
-          </Link>
-        </div>
-      </nav>
-    </header>
-  );
+    const isMobile = useMediaQuery({ query: `(max-width:800px)` });
+
+    return (
+        <header>
+            <nav className="navbar bg-body-tertiary container">
+                {isMobile && (
+                    <div className="container d-flex align-items-center justify-content-between ">
+                        <button
+                            className="navbar-toggler navbar-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <CiMenuBurger />
+                        </button>
+                        <div className="logo-container">
+                            <NavLink to="" className="d-flex flex-column align-items-center ">
+                                <PiBoneThin className="logo navbar-brand" />
+                                <h4 className="logo-text">Parker & Co.</h4>
+                            </NavLink>
+                        </div>
+                        <button className="navbar-button">
+                            <CiShoppingCart />
+                        </button>
+                        <div className="collapse navbar-collapse " id="navbarNav">
+                            <ul className="navbar-nav mobile-navbar justify-content-evenly">
+                                <li className="fs-3">
+                                    <NavLink to="">Home</NavLink>
+                                </li>
+
+                                <li className="fs-3">
+                                    <NavLink to="">Shop By Pet</NavLink>
+                                </li>
+
+                                <li className="fs-3">
+                                    <NavLink to="">Wear</NavLink>
+                                </li>
+
+                                <li className="fs-3">
+                                    <NavLink to="">Play</NavLink>
+                                </li>
+
+                                <li className="fs-3">
+                                    <NavLink to="">Walk</NavLink>
+                                </li>
+
+                                <li className="fs-3">
+                                    <NavLink to="">Eat</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    // {isDisplay && (
+                    //     <ul>
+                    //         <li>
+                    //             <NavLink to="">Home</NavLink>
+                    //         </li>
+                    //         <li>
+                    //             <NavLink to="">Shop By Pet</NavLink>
+                    //         </li>
+                    //         <li>
+                    //             <NavLink to="">Wear</NavLink>
+                    //         </li>
+
+                    //         <li>
+                    //             <NavLink to="">Play</NavLink>
+                    //         </li>
+                    //         <li>
+                    //             <NavLink to="">Walk</NavLink>
+                    //         </li>
+                    //         <li>
+                    //             <NavLink to="">Eat</NavLink>
+                    //         </li>
+                    //     </ul>
+                    // )}
+                )}
+                {!isMobile && (
+                    <ul>
+                        <li>
+                            <NavLink to="">Shop By Pet</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">Wear</NavLink>
+                        </li>
+                        <li>
+                            <div>
+                                <NavLink to="">
+                                    <PiBoneThin className="logo navbar-brand" />
+                                </NavLink>
+                            </div>
+                        </li>
+                        <li>
+                            <NavLink to="">Play</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">Walk</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="">Eat</NavLink>
+                        </li>
+                    </ul>
+                )}
+            </nav>
+        </header>
+    );
 }
